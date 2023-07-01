7a57b3cc56a75a72a846b710cf052d607a757b65
horek <azenoxe@gmail.com>
2023-07-01 10:34:48 +0200

    feat: add vitest with basic test to setup environnement

---
+++ vitestSetup.ts
@@ -0,0 +1,7 @@
+// vitestSetup.ts
+import { beforeAll, vi } from "vitest";
+
+beforeAll(() => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+  vi.mock("next/router", () => require("next-router-mock"));
+});
