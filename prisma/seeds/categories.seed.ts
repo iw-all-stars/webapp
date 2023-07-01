925295193cb5bcf457574602287a0043b53cc4b5
horek <azenoxe@gmail.com>
2023-07-01 03:02:04 +0200

    feat: add default seeds for categories

---
+++ prisma/seeds/categories.seed.ts
@@ -0,0 +1,38 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+export default async function seedCategories() {
+
+  const categoriesNames = [
+    "Africain",
+    "Américain",
+    "Asiatique",
+    "Bio",
+    "Burger",
+    "Chinois",
+    "Coréen",
+    "Cuisine du monde",
+    "Espagnol",
+    "Français",
+    "Grec",
+    "Indien",
+    "Italien",
+    "Japonais",
+    "Libanais",
+    "Marocain",
+    "Mexicain",
+    "Haïtien",
+    "Sushi",
+    "Thaïlandais",
+    "Turc",
+    "Végétarien",
+    "Vietnamien",
+  ];
+
+  const seedCategories = await prisma.category.createMany({
+    data: categoriesNames.map((name) => ({
+      name 
+    })),
+  });
+
+	console.log({ seedCategories });
+}
