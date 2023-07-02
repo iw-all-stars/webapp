3d84fda44a1d552e29279ccce1c64fe7a8364fee
FantinPro <raimbault.fantin.pro@gmail.com>
2023-07-02 16:20:03 +0200

    remove vitest

--- vitestSetup.ts
+++
@@ -1,7 +0,0 @@
-// vitestSetup.ts
-import { beforeAll, vi } from "vitest";
-
-beforeAll(() => {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-  vi.mock("next/router", () => require("next-router-mock"));
-});
