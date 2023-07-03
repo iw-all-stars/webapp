b40ed403f18ee15fedb4ea106fbee54570144612
horek <azenoxe@gmail.com>
2023-07-03 14:38:12 +0200

    feat: add elasticsearch to docker-compose, add CUD for clients index in readme and add hook to sync clients table to clients index in elk

--- README.md
+++ README.md
@@ -39,3 +39,44 @@ yarn dev
 ```bash
 docker build -t webapp --build-arg NEXT_PUBLIC_CLIENTVAR=clientvar --build-arg SKIP_ENV_VALIDATION=true --build-arg NEXTAUTH_URL=http://localhost:3000 --build-arg NEXTAUTH_SECRET=one-piece .
 ```
+
+
+
+# ELK
+
+## Create client index
+```
+curl -X PUT -H 'Content-Type: application/json' http://localhost:9200/clients -u elastic:${ELASTIC_PASSWORD} -d '{
+   "mappings": {
+     "properties": {
+       "email": { "type": "keyword"  },
+       "name": { "type": "keyword" },
+       "firstname": { "type": "keyword" },
+       "phone": { "type": "keyword" },
+       "image": { "type": "keyword" },
+       "address": { "type": "keyword" },
+       "city": { "type": "keyword" },
+       "unsubscribed": { "type": "keyword" },
+       "zip": { "type": "keyword" },
+       "Mail": { "type": "keyword" },
+       "lastLogin": { "type": "date" },
+       "createdAt": { "type": "date" },
+       "updatedAt": { "type": "date" }
+     }
+   }
+}'
+```
+
+## Search data in client index
+```
+curl http://localhost:9200/clients/_search -u elastic:${ELASTIC_PASSWORD}
+```
+
+## Delete data in client index
+```
+curl -XPOST 'http://localhost:9200/clients/_delete_by_query' -H 'Content-Type: application/json' -u elastic:${ELASTIC_PASSWORD} -d '{
+    "query" : {
+        "match_all" : {}
+    }
+}'
+```
--- src/server/db.ts
+++ src/server/db.ts
@@ -1,7 +1,15 @@
-import { type PrismaClient } from "@prisma/client";
+import { type PrismaClient, type Client as ModelClient } from "@prisma/client";
 import prismaInstance from './client.prisma';
-
+import { Client as ClientElk } from "@elastic/elasticsearch";
 import { env } from "../env.mjs";
+
+const clientElk = new ClientElk({
+    node: "http://localhost:9200",
+    auth: {
+        username: "elastic",
+        password: "elastic_password"
+    }
+})
 
 const globalForPrisma = globalThis as unknown as {
     prisma: PrismaClient | undefined;
@@ -9,7 +17,51 @@ const globalForPrisma = globalThis as unknown as {
 
 const prismaClient =
     globalForPrisma.prisma ??
-    prismaInstance
+    prismaInstance;
+
+prismaClient.$use(async (params, next) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const result = await next(params);
+
+  if (params.model === "Client") {
+    if (params.action === "create") {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        const newClient: ModelClient = structuredClone(result);
+
+        const { id, ...restNewClient } = newClient;
+        // console.log('test', id)
+        await clientElk.create({
+            index: "clients",
+            id,
+            document: { ...restNewClient }
+        })
+    } else if (params.action === "update") {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        const updatedClient: ModelClient = structuredClone(result);
+
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        const { id, ...restUpdatedClient } = updatedClient;
+        await clientElk.update({
+            index: "clients",
+            id,
+            doc: { ...restUpdatedClient }
+        })
+    } else if (params.action === "delete") {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        const deletedClient: ModelClient = result;
+
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        const { id } = deletedClient;
+        await clientElk.delete({
+            index: "clients",
+            id,
+        })
+    }
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+  return result;
+});
 
 export const prisma = prismaClient;
 
